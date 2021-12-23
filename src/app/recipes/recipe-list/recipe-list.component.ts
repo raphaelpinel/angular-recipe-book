23cc62b7c4cd2b46bbdf32bf2c1279e9a8c11373
Raphaël Pinel <raphaelpinel@gmail.com>
2021-12-23 20:23:48 +0200

    Add first recipe in the list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is just a test',
+      'https://c.pxhere.com/photos/41/62/close_up_cucumber_cuisine_delicious_dinner_dish_fish_food-1557745.jpg!d'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
